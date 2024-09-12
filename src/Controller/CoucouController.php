ed2e5455429f0173b91cf6fd913b0861057f3294
WebDevCF2m <gitweb@cf2m.be>
2024-09-12 13:09:58 +0200

    make:controller CoucouController

---
+++ src/Controller/CoucouController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class CoucouController extends AbstractController
+{
+    #[Route('/coucou', name: 'app_coucou')]
+    public function index(): Response
+    {
+        return $this->render('coucou/index.html.twig', [
+            'controller_name' => 'CoucouController',
+        ]);
+    }
+}

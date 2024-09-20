39511e2f93b09977dce6fb5e2dd160967a868413
WebDevCF2m <gitweb@cf2m.be>
2024-09-20 16:28:05 +0200

    make:crud with admin

--- src/Controller/CoucouController.php
+++ src/Controller/CoucouController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+# on va charger le Repository (manager) de Section
+use App\Repository\SectionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +11,23 @@ use Symfony\Component\Routing\Attribute\Route;
 class CoucouController extends AbstractController
 {
     #[Route('/', name: 'coucou')]
-    public function index(): Response
+    public function index(SectionRepository $sections): Response
     {
         return $this->render('coucou/index.html.twig', [
             'title' => 'Homepage',
+            'sections' => $sections->findAll(),
+        ]);
+    }
+
+    # A FAIRE
+    #[Route('/section/{id}', name: 'section')]
+    public function section(int $id, SectionRepository $sections): Response
+    {
+        $section = $sections->findOneBy(['id' => $id]);
+        return $this->render('coucou/section.html.twig', [
+            'title' => $section->getSectionTitle(),
+            'section' => $section,
+            'sections' => $sections->findAll(),
         ]);
     }
 }
